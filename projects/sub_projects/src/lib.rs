4d6fc67e4658a17572fc104326d914221289218f
SasakiSaki <galaster@foxmail.com>
2023-05-06 16:46:23 +0800

    🎯 Deny missing copy and debug structure (#9)

--- projects/sub_projects/src/lib.rs
+++ projects/sub_projects/src/lib.rs
@@ -1,3 +1,9 @@
+#![deny(missing_debug_implementations, missing_copy_implementations)]
+#![warn(missing_docs, rustdoc::missing_crate_level_docs)]
+#![doc = include_str!("../readme.md")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
+#![doc(html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
+
 mod errors;
 
-pub use errors::{Error, Result};
+pub use crate::errors::{Error, Result};
